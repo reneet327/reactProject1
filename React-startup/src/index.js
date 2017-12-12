102f865f16f10b339ee3e87938a8d30eb0d0281f
reneet327 <reneet327@gmail.com>
2017-12-12 14:56:01 -0800

    This is my official project 1 that was created by using nesting in react - not the best way to code in react...

--- React-startup/src/index.js
+++ React-startup/src/index.js
@@ -1,1 +1,18 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement('table', null,
+    React.createElement('tr', null,
+      React.createElement('th', null, 'First Name'),
+      React.createElement('th', null, 'Last Name')),
+    React.createElement('tr', null,
+      React.createElement('td', null, 'Lisa'),
+      React.createElement('tr', null, 'Jones')),
+    React.createElement('tr', null,
+      React.createElement('td', null, 'Bill'),
+      React.createElement('tr', null, 'Jaffey'))
+
+
+    )  ,document.getElementById('root')
+);
